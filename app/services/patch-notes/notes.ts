7833a3a99319014d06189623fbe5b4438771a03c
Akihiko Koizuka <koizuka@dwango.co.jp>
2020-11-27 15:29:18 +0900

    v1.0.20201127-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20201124-unstable.1',
-  title: '1.0.20201124-unstable.1',
+  version: '1.0.20201127-unstable.1',
+  title: '1.0.20201127-unstable.1',
   notes: [
-    '修正: 予約されたユーザー生放送でテスト配信できない不具合を修正 (#474)'
+    '修正: 2回目の範囲指定画面キャプチャで操作不能になる問題を回避 (#477)',
+    '操作画面が半透明にならない問題はまだ調査中ですが、取り急ぎ、操作不能になる問題を回避します。'
   ]
 };
