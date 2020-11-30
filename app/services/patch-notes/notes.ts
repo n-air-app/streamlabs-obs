422f33b0db66ee1b6daa4b4a6c17736a4fea8961
Akihiko Koizuka <koizuka@dwango.co.jp>
2020-11-30 15:10:38 +0900

    v1.0.20201130-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,10 +1,9 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20201127-unstable.1',
-  title: '1.0.20201127-unstable.1',
+  version: '1.0.20201130-unstable.1',
+  title: '1.0.20201130-unstable.1',
   notes: [
-    '修正: 2回目の範囲指定画面キャプチャで操作不能になる問題を回避 (#477)',
-    '操作画面が半透明にならない問題はまだ調査中ですが、取り急ぎ、操作不能になる問題を回避します。'
+    '修正: 範囲指定画面キャプチャ時のタイトルバーと不透明な背景を除去 (#479)'
   ]
 };
