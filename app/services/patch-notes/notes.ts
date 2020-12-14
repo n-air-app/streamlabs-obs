87c6269ec7c102753976e1d57991d0c8b50cd9e1
Akihiko Koizuka <koizuka@dwango.co.jp>
2020-12-14 13:49:30 +0900

    v1.0.20201214-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.0.20201130-unstable.1',
-  title: '1.0.20201130-unstable.1',
+  version: '1.0.20201214-unstable.1',
+  title: '1.0.20201214-unstable.1',
   notes: [
-    '修正: 範囲指定画面キャプチャ時のタイトルバーと不透明な背景を除去 (#479)'
+    '修正: 番組延長が停止メンテナンス前でエラーになったときのエラーメッセージを追加 (#482)',
+    '修正: 範囲指定画面キャプチャが下端を指定できなかった (#480)'
   ]
 };
