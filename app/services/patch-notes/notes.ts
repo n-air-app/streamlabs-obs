27ff60b81e207d43316d5c8f6d18361e1ddfbcdf
Akihiko Koizuka <koizuka@dwango.co.jp>
2021-03-18 12:12:39 +0900

    v1.1.20210318-unstable.1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,9 +1,10 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20201222-unstable.1',
-  title: '1.1.20201222-unstable.1',
+  version: '1.1.20210318-unstable.1',
+  title: '1.1.20210318-unstable.1',
   notes: [
-    '修正: プロジェクターを2つ以上開けるようにする(obs-studio-nodeを0.3.43に更新) (#485)'
+    '修正: ニコ生番組作成後、作成ウィンドウが閉じなくなっていた (#495)',
+    '修正: 番組説明文にあるリンクをクリックしたときにアプリの画面が置き換わってしまっていた (#491)'
   ]
 };
