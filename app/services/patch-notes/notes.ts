46ebb9469cde619be01da8f57591d2cb2c5fbc41
Akihiko Koizuka <koizuka@dwango.co.jp>
2021-05-26 14:02:54 +0900

    v1.1.20210526-1

--- app/services/patch-notes/notes.ts
+++ app/services/patch-notes/notes.ts
@@ -1,11 +1,11 @@
 import { IPatchNotes } from '.';
 
 export const notes: IPatchNotes = {
-  version: '1.1.20210318-1',
-  title: '1.1.20210318-1',
+  version: '1.1.20210526-1',
+  title: '1.1.20210526-1',
   notes: [
-    '修正: ニコ生番組作成後、作成ウィンドウが閉じなくなっていた (#495) by koizuka',
-    '修正: プロジェクターを2つ以上開けるようにする(obs-studio-nodeを0.3.43に更新) (#485) by koizuka',
-    '修正: 番組説明文にあるリンクをクリックしたときにアプリの画面が置き換わってしまっていた (#491) by koizuka'
+    '追加: ニコニコ生放送のコメント読み上げ機能 (#494)',
+    '変更: 利用規約の「niconico」を「ニコニコ」に改訂 (#499)',
+    '開発: stable releaseのpatch-noteから authorを削除 (#496)'
   ]
 };
